00e1fd44b1767d0dd92892d9a6486c1bde84dc89
Garth Johnson <growlfd@gmail.com>
2026-08-26 15:01:16 -0700

    chore: bump version to 0.2.5 (#270)

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 ###############################################################################
 set -euo pipefail
 
-readonly VERSION="0.2.4"
+readonly VERSION="0.2.5"
 readonly STATE_DIR="$HOME/.opskit-install/state"
 readonly OPSKIT_DIR="${OPSKIT_DIR:-$(git rev-parse --show-toplevel 2>/dev/null || echo "$HOME/Projects/opskit")}"
 OPSKIT_BIN="${OPSKIT_BIN:-$OPSKIT_DIR/bin/opskit}"
